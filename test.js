29e3c7909b394d2c2cee0d2b7bf8d1fae7395858
Tania <evdunova.tania@gmail.com>
2023-01-29 20:53:14 +0100

    kata2

--- test.js
+++ test.js
@@ -29,3 +29,50 @@ function getDivisorsCnt(n){
   }
   return count;
 }
+/// Kata Especially Joyful Numbers
+function numberJoy(n) {
+  let str = String(n)
+  let sum = 0;
+  for (let i = 0; i < str.length;i++) {
+     sum += Number(str[i])
+  }
+  let num = String(sum).split('').reverse().join('')
+  if (sum * Number(num) === n) {
+    return true;
+  } else {
+    return false
+  }
+  
+    // your code
+}
+//// Kata Convert boolean values to strings 'Yes' or 'No'.
+function boolToWord( bool ){
+  if (bool) {
+    return 'Yes'
+  } else { 
+  return 'No'}
+  //...
+}
+//// Kata  Sum two arrays
+function addArrays(array1, array2) {
+  if (array1.length === 0) {
+    return array2
+  } else if (array2.length === 0) {
+    return array1
+  }
+  let arrayToNumber1 = parseInt(array1.join(''));
+  let arrayToNumber2 = parseInt(array2.join(''));
+  let arr = arrayToNumber1 + arrayToNumber2;
+ 
+  let str = String(arr);
+  let arr2 =[];
+  for (let i =0;i<str.length;i++) {
+    arr2.push(Number(str[i]))
+  }
+  if (isNaN(arr2[0])) {
+    arr2.shift()
+    arr2[0] = arr2[0] * (-1)
+  }
+  return arr2
+  
+}
